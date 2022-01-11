549b1161f0a122f9a1c429cb5388a628354869d8
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-01-11 08:47:11 -0400

    Initial commit

---
+++ Assets/CubeRotator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * 90f * Time.deltaTime);
+        gameObject.transform.Rotate(Vector3.right * 45f * Time.deltaTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Level-Programming-Test
+ 
